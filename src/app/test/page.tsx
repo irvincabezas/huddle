24a980d5ede979b303e1704c543bf6dcb7266ff1
irvincabezas <irvin.cabezas@gmail.com>
2025-09-15 21:05:32 -0600

    Added Convex Auth support 49.17

---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+
+const Page = () => {
+  return <div>Test Page</div>;
+};
+
+export default Page;
