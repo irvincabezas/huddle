3ea59d12fa2005147a02b7bddfd220fd9ef03b04
irvincabezas <irvin.cabezas@gmail.com>
2025-09-24 01:46:34 -0600

    Added a few fixes and started implementing workspaces 9.10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
 "use client";
 
 import { UserButton } from "@/features/auth/components/user-button";
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+import { useEffect, useMemo } from "react";
 
 export default function Home() {
+  const { data, isLoading } = useGetWorkspaces();
+  const workspaceId = useMemo(() => data?.[0]?._id, [data]);
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    if (workspaceId) {
+      console.log("Redirect to workspace", workspaceId);
+    } else {
+      console.log("Open creation modal");
+    }
+  }, [workspaceId, isLoading]);
+
   return (
     <div>
       <UserButton />
