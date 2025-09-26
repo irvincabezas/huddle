6a2eb5415ded91c76b086742395dfac4b5156a16
irvincabezas <irvin.cabezas@gmail.com>
2025-09-25 23:59:34 -0600

    Atom configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { UserButton } from "@/features/auth/components/user-button";
+import { useCreateWorkspaceModal } from "@/features/store/use-create-workspace-modal";
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useEffect, useMemo } from "react";
 
 export default function Home() {
+  const [open, setOpen] = useCreateWorkspaceModal();
   const { data, isLoading } = useGetWorkspaces();
   const workspaceId = useMemo(() => data?.[0]?._id, [data]);
 
